7157f40414a4eb92d59ea6c5512f894e0d62d39b
Alan Emiliano Alcaraz Rocha <alcarazemi2003@gmail.com>
2024-10-10 21:08:24 -0600

    primer commit 10 oct

---
+++ src/app/formulario/ejemplo1/ejemplo1.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-ejemplo1',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './ejemplo1.component.html',
+  styles: ``
+})
+export class Ejemplo1Component implements OnInit {
+
+  formGroup!: FormGroup;
+
+  constructor(private readonly fb: FormBuilder){}
+
+  ngOnInit(): void {
+      this.formGroup = this.initForm();
+  }
+
+  initForm():FormGroup{
+    return this.fb.group({
+      email:[''],
+      password:['']
+    })
+  }
+
+  onSubmit():void{
+    console.log('Form ->', this.formGroup.value)
+  }
+
+
+}
